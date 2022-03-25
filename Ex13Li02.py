e11a7fb6c5773adbddbd4e94ab504f29835bb136
emersonnfs <101301360+emersonnfs@users.noreply.github.com>
2022-03-25 19:50:31 -0300

    Add files via upload
    Exercicio 12 de Python

---
+++ Ex13Li02.py
@@ -0,0 +1,9 @@
+nac = float(input("Informe qual foi sua nota na NAC: "))
+
+am= float(input("Informe qual foi sua nota na AM: "))
+
+ps = float(input("Informe qual foi sua nota na PS: "))
+
+media = ((0.2 * nac) + (0.3 * am) + (0.5 * ps) )
+
+print("A sua média semestral foi: " + str(media))
